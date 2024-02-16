2a772306feee762c46e60ffcbfdb4697507633e4
khushi303 <maankhushi303@gmail.com>
2024-02-16 10:17:16 +0530

    deploying

---
+++ src/components/FormPage.jsx
@@ -0,0 +1,55 @@
+import React, { useRef, useState } from 'react';
+import emailjs from '@emailjs/browser';
+
+const FormPage = () => {
+    const form = useRef();
+    const [name, setName] = useState("")
+    const [email, setEmail] = useState("")
+    const [contact, setContact] = useState("")
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs
+            .sendForm('service_31h180h', 'template_rxq3f9e', form.current, {
+                publicKey: '1cJ95ExJ1P3ygeRiD',
+            })
+            .then(
+                () => {
+                    console.log('SUCCESS!');
+                    e.target.reset();
+                },
+                (error) => {
+                    console.log('FAILED...', error.text);
+                },
+            );
+    };
+    return (
+        <div className='bg-teal-100 min-h-screen pt-[50px]'>
+            <h2 className='text-[60px] font-normal text-lime-950 text-center'>FORM</h2>
+            <div className='container mx-auto px-3 xl:max-w-[1120px]'>
+                <form ref={form} onSubmit={sendEmail} className='w-full flex flex-col justify-center items-center gap-6'>
+                    <div className='w-full'>
+                        <label className='text-lime-950 text-[25px] font-normal'>Name</label>
+                        <input type="text" name="user_name" value={name} onChange={(e) => setName(e.target.value)} required className='border-[2px] border-solid border-black bg-orange-100 w-full rounded-[8px] p-[10px]' />
+                    </div>
+                    <div className='w-full'>
+                        <label className='text-lime-950 text-[25px] font-normal'>Contact</label>
+                        <input type="text" name='user_contact' value={contact} maxLength={10} minLength={10} onChange={(e) => setContact(e.target.value)} required className='border-[2px] bg-orange-100 border-solid border-black w-full rounded-[8px] p-[10px]' />
+                    </div>
+                    <div className='w-full'>
+                        <label className='text-lime-950 text-[25px] font-normal'>Email</label>
+                        <input type="email" name="user_email" value={email} onChange={(e) => setEmail(e.target.value)} required className='border-[2px] border-solid bg-orange-100 border-black w-full rounded-[8px] p-[10px]' />
+                    </div>
+                    <div className='w-full'>
+                        <label className='text-lime-950 text-[25px] font-normal'>Message</label>
+                        <textarea name="message" className='border-[2px] bg-orange-100 border-solid border-black w-full rounded-[8px] p-[10px]' required />
+                    </div>
+                    <input type="submit" value="Send" className='border border-solid text-white text-[25px] font-normal bg-lime-950 border-lime-950 cursor-pointer py-[12px] px-[40px] rounded-[12px]' />
+                </form>
+            </div>
+        </div>
+    )
+}
+
+export default FormPage
---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import Header from '../components/Header';
+import ModalSec from '../components/ModalSec';
+import { Route, Routes } from 'react-router-dom';
+import FormPage from '../components/FormPage';
+import AccSec from '../components/AccSec';
+import Navbar from '../components/Navbar';
+import SlickSlider from '../components/SlickSlider';
+import ReducerCounter from '../components/ReducerCounter';
+
+const Homepage = () => {
+    return (
+        <div>
+            <Navbar />
+            <Routes>
+                <Route path='/' exact element={<Header />}></Route>
+                <Route path='/Modal' exact element={<ModalSec />}></Route>
+                <Route path='/FormPage' exact element={<FormPage />}></Route>
+                <Route path='/AccSec' exact element={<AccSec />}></Route>
+                <Route path='/SlickSlider' exact element={<SlickSlider />}></Route>
+                <Route path='/ReducerCounter' exact element={<ReducerCounter />}></Route>
+            </Routes>
+        </div>
+    )
+}
+
+export default Homepage
